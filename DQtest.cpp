de8f0b54f07d4fab2e9211296dd41a9bb9e1d6de
CharlesZhang <Charleszhang4@gmail.com>
2024-02-04 15:46:01 -0800

    Test if the merge was correct

--- DQtest.cpp
+++ DQtest.cpp
@@ -8,16 +8,13 @@ int main(){
 
     const int N = 8;
 
-    Deque<int> q;
+    Deque<char> q;
 
     for (int j = 0; j < N; j++){
-        q.enqueue(j);
+        q.enqueue('j');
     }
-    q.reserve(9);
-    for (int i = 0; i < 8; i++){
-        q.dequeue();
-    }
-    q.ddisplay();
-    cout << q.dequeue() << endl;
-    q.ddisplay();
+    q.display();
+
+    cout << q[0];
+
 }
